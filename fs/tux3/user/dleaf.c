ea73bf8b8a2083e21c153dad285cc4ae9dc37c3a
Daniel Phillips <daniel@tux3.org>
2008-12-13 03:22:00 -0800

    tux3: Remove __KERNEL__ from kernel/dleaf.c. Also write ERR_PTR(PTR_ERR()) instead of ERR_CAST, for older kernels
    Signed-off-by: Daniel Phillips <daniel@tux3.org>

--- fs/tux3/user/dleaf.c
+++ fs/tux3/user/dleaf.c
@@ -19,7 +19,12 @@
 #define trace trace_on
 #endif
 
-#include "tux3.h"	/* include user/tux3.h, not fs/tux3/tux3.h */
+#include "tux3.h"
+#ifndef main
+#define MAX_GROUP_ENTRIES 7
+#else
+#define MAX_GROUP_ENTRIES 255
+#endif
 #include "../dleaf.c"
 
 #ifndef main
