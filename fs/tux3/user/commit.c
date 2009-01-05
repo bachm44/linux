0e110e76e674ead21c6732156471f7c8da7a3d06
Daniel Phillips <daniel@tux3.org>
2009-01-05 00:40:54 -0800

    tux3: Add logblock->prevlog field to reference previous log block in delta log chain
    Signed-off-by: Daniel Phillips <daniel@tux3.org>

--- fs/tux3/user/commit.c
+++ fs/tux3/user/commit.c
@@ -16,7 +16,7 @@
 #include "../hexdump.c"
 #include "../balloc.c"
 
-struct logblock { be_u16 magic, bytes; unsigned char data[]; };
+struct logblock { be_u16 magic, bytes; be_u64 prevlog; unsigned char data[]; };
 
 enum { LOG_ALLOC, LOG_FREE };
 
