ff0039f920105e949cf1ee9704905b3effcabdc1
Bartłomiej Chmiel <bachm44@gmail.com>
2022-09-27 11:34:35 +0200

    Fs boilerplate

---
+++ fs/ext2-inc/ext2-inc.c
@@ -0,0 +1,168 @@
+#include <linux/fs.h>
+#include <linux/init.h>
+#include <linux/kernel.h>
+#include <linux/module.h>
+#include <linux/err.h>
+#include <linux/buffer_head.h>
+#include <linux/time64.h>
+
+#include "ext2-inc.h"
+
+int ext2_statfs(struct dentry *dentry, struct kstatfs *stat)
+{
+	return simple_statfs(dentry, stat);
+}
+
+int ext2_drop_inode(struct inode *inode)
+{
+	return generic_delete_inode(inode);
+}
+
+static struct super_operations s_op = {
+	// .alloc_inode = ext2_alloc_inode,
+	// .destroy_inode = ext2_destroy_inode,
+	// .free_inode = ext2_free_inode,
+	// .dirty_inode = ext2_dirty_inode,
+	// .write_inode = ext2_write_inode,
+	// .drop_inode = ext2_drop_inode,
+	// .evict_inode = ext2_evict_inode,
+	// .put_super = ext2_put_super,
+	// .sync_fs = ext2_sync_fs,
+	// .freeze_super = ext2_freeze_super,
+	// .freeze_fs = ext2_freeze_fs,
+	// .thaw_super = ext2_thaw_super,
+	// .unfreeze_fs = ext2_unfreeze_fs,
+	// .statfs = ext2_statfs,
+	// .remount_fs = ext2_remount_fs,
+	// .umount_begin = ext2_umount_begin,
+	// .show_options = ext2_show_options,
+	// .show_devname = ext2_show_devname,
+	// .show_path = ext2_show_path,
+	// .show_stats = ext2_stats,
+	// .bdev_try_to_free_page = ext2_bdev_try_to_free_page,
+	// .nr_cached_objects = ext2_nr_cached_objects,
+	// .free_cached_objects = ext2_free_cached_objects
+	.statfs = ext2_statfs,
+	.drop_inode = ext2_drop_inode
+};
+
+#define EXT2_SUPERBLOCK_BLOCK_NUMBER 1
+#define EXT2_DEFAULT_BLOCK_SIZE 1024
+#define EXT2_ROOT_INODE_NUMBER 1
+
+struct inode *ext2_make_inode(struct super_block *sb)
+{
+	return NULL;
+}
+
+static const struct inode_operations ext2_inode_ops = {};
+static const struct file_operations ext2_file_ops = {};
+
+struct ext2_inode *ext2_get_inode(struct super_block *sb, int inode_number)
+{
+	return NULL;
+}
+
+int ext2_fill_super(struct super_block *sb, void *data, int silent)
+{
+	int ret = -EPERM;
+	struct buffer_head *bh;
+	struct ext2_super_block *sb_disk;
+	struct inode *root = ext2_make_inode(sb);
+
+	bh = sb_bread(sb, EXT2_SUPERBLOCK_BLOCK_NUMBER);
+	BUG_ON(!bh);
+
+	sb_disk = (struct ext2_super_block *)bh->b_data;
+
+	pr_info("Ext2 magic number in disk is: [0x%X]", sb_disk->s_magic);
+
+	if (unlikely(sb_disk->s_magic != EXT2_MAGIC)) {
+		pr_err("Magic number mismatch when mounting ext2");
+		goto release;
+	}
+
+	pr_info("Superblock block size: [%d]", super_block_block_size(sb_disk));
+
+	if (unlikely(super_block_block_size(sb_disk) !=
+		     EXT2_DEFAULT_BLOCK_SIZE)) {
+		pr_err("Ext2 not mounted with standard block size");
+		goto release;
+	}
+
+	sb->s_magic = EXT2_MAGIC;
+	sb->s_fs_info = sb_disk;
+	sb->s_maxbytes = EXT2_DEFAULT_BLOCK_SIZE;
+	sb->s_op = &s_op;
+
+	root->i_ino = EXT2_ROOT_INODE_NUMBER;
+	inode_init_owner(NULL, root, NULL, S_IFDIR);
+	root->i_sb = sb;
+	root->i_op = &ext2_inode_ops;
+	root->i_fop = &ext2_file_ops;
+	root->i_atime = ns_to_timespec64(0);
+	root->i_mtime = ns_to_timespec64(0);
+	root->i_ctime = ns_to_timespec64(0);
+	root->i_private = ext2_get_inode(sb, EXT2_ROOT_INODE_NUMBER);
+
+	sb->s_root = d_make_root(root);
+
+	if (!sb->s_root) {
+		ret = -ENOMEM;
+		pr_err("Memory error when making root inode");
+		goto release;
+	}
+
+	ret = 0;
+
+release:
+	brelse(bh);
+
+	return ret;
+}
+
+struct dentry *ext2_mount(struct file_system_type *fs_type, int flags,
+			  const char *dev_name, void *data)
+{
+	// TODO create here superblock
+
+	struct dentry *ret;
+	ret = mount_bdev(fs_type, flags, dev_name, data, ext2_fill_super);
+
+	if (unlikely(IS_ERR(ret)))
+		pr_err("ext2 failed to mount.");
+	else
+		pr_info("ext2 mounted on [%s]", dev_name);
+
+	return ret;
+}
+
+void ext2_kill_sb(struct super_block *sb)
+{
+	pr_info("ext2 block destroyed.");
+	kill_block_super(sb);
+}
+
+static struct file_system_type fs_type = { .name = "ext2-inc",
+					   .fs_flags = 0,
+					   .mount = ext2_mount,
+					   .kill_sb = ext2_kill_sb,
+					   .owner = THIS_MODULE,
+					   .next = NULL };
+
+static int __init ext2_init(void)
+{
+	return register_filesystem(&fs_type);
+}
+
+static void __exit ext2_exit(void)
+{
+	unregister_filesystem(&fs_type);
+}
+
+module_init(ext2_init);
+module_exit(ext2_exit);
+
+MODULE_AUTHOR("Bartłomiej Chmiel <incvis@protonmail.com");
+MODULE_LICENSE("GPL");
+MODULE_INFO(intree, "Y");
---
+++ fs/ext2-inc/ext2-inc.h
@@ -0,0 +1,102 @@
+#ifndef EXT2_INC_H
+#define EXT2_INC_H
+
+#include <linux/types.h>
+
+typedef __u8 u8;
+typedef __u16 u16;
+typedef __u32 u32;
+
+// #define pr_fmt(fmt) "%s:%s: " fmt, KBUILD_MODNAME, __func__
+
+#define SUPERBLOCK_OFFSET 1024
+#define EXT2_MAGIC 0x55552222
+
+#pragma pack(push, 1)
+struct ext2_inode {
+	u16 i_mode; // type_permissions
+	u16 i_uid; // user_id
+	u32 i_size; // lower_size_bytes
+	u32 i_atime; // last_access_time
+	u32 i_ctime; // creation_time
+	u32 i_mtime; // last_modification_time
+	u32 i_dtime; // deletion_time
+	u16 i_gid; // group_id
+	u16 i_links_count; // hard_links_count
+	u32 i_blocks; // disk_sectors_in_use_count
+	u32 i_flags;
+	u32 i_osd1; // os_specific_value
+	u32 i_block[15];
+	u32 i_generation;
+	u32 i_file_acl;
+	u32 i_dir_acl;
+	u32 i_faddr; // fragment_block_address
+	u32 i_osd2; // os_specific_value
+};
+
+struct ext2_super_block {
+	u32 s_inodes_count; // total_inodes
+	u32 s_blocks_count; // total_blocks
+	u32 s_r_blocks_count; // blocks_reserved_for_superuser
+	u32 s_free_blocks_count; // unallocated_blocks
+	u32 s_free_inodes_count; // unallocated_inodes
+	u32 s_first_data_block; // block_number_of_superblock
+	u32 s_log_block_size; // log2_block_size
+	u32 s_log_frag_size; // log2_fragment_size
+	u32 s_blocks_per_group; // blocks_in_each_block_group
+	u32 s_frags_per_group; // fragments_in_each_block_group
+	u32 s_inodes_per_group; // inodes_in_each_block_grup
+	u32 s_mtime; // last_mount_time
+	u32 s_wtime; // last_written_time
+	u16 s_mnt_count; // times_volume_mounted_since_consistency_check
+	u16 s_max_mnt_count; // mounts_allowed_before_consistency_check
+	u16 s_magic; // ext2_signature
+	u16 s_state; // file_system_state
+	u16 s_errors; // error_handling
+	u16 s_minor_rev_level; // minor_version
+	u32 s_lastcheck; // last_consistency_check
+	u32 s_checkinterval; // time_between_consistency_checks
+	u32 s_creator_os; // creator_os_id
+	u32 s_rev_level; // major_version
+	u16 s_def_resuid; // reserved_blocks_user_id
+	u16 s_def_resgid; // reserved_blocks_group_id
+
+	//extended superblock fields
+	u32 s_first_ino; // first_non_reserved_inode
+	u16 s_inode_size;
+	u16 s_block_group_nr; // block_group_of_this_superblock
+	u32 s_feature_compat; /// optional_features
+	u32 s_feature_incompat; // required_features
+	u32 s_feature_ro_compat; // non_supported_features
+	u8 s_uuid[16];
+	u8 s_volume_name[16];
+	u8 s_last_mounted[64]; // last_mounted_on_path
+	u32 s_algo_bitmap; // compression_algorithms
+	u8 s_prealloc_blocks; // file_blocks_to_preallocate
+	u8 s_prealloc_dir_blocks; // directories_blocks_to_preallocate
+	u16 unused;
+	u8 s_journal_uuid[16];
+	u32 s_journal_inum; // journal_inode
+	u32 s_journal_dev; // journal_device
+	u32 s_last_orphan; // head_of_orphan_inode_list
+	u8 unused2[788];
+};
+
+inline u32 super_block_block_size(struct ext2_super_block *sb)
+{
+	return 1 << sb->s_log_block_size;
+}
+
+struct ext2_block_group_descriptor {
+	u32 bg_block_bitmap; // block_bitmap_address
+	u32 bg_inode_bitmap; // inode_bitmap_address
+	u32 bg_inode_table; // inode_table_starting_block
+	u16 bg_free_blocks_count; // unallocated_blocks_in_group
+	u16 bg_free_inodes_count; // unallocated_inodes_in_group
+	u16 bg_used_dirs_count; // directories_in_group
+	u16 padding;
+	u8 reserved[12];
+};
+#pragma pack(pop)
+
+#endif
